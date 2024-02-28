797805e4fbc0b84dca2d17ffaea3f7d91aad2fd9
Garyy12 <90533473+Garyy12@users.noreply.github.com>
2024-02-27 19:15:17 -0600

    Add files via upload

---
+++ blogs/las_tumbas_10.php
@@ -0,0 +1,75 @@
+<?php
+$titulo = "Las tumbas 10";
+$descripcionImg = "No se que era";
+$contenido = "<div>Son tumbas</div>";
+
+$descripcion = "tumbas";
+
+$rutaDestino = "../img/175496196-día-de-prevención-de-desastres-naturales-con-niños-explicando-guía-de-seguridad.jpg";
+
+$autor = "Yo xd";
+$keywords = "tumbas";
+$estado = "Tijuana";
+?> 
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title><?php echo $titulo ?></title>
+      <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+  
+      <link rel="stylesheet" href="../css/styles.css">
+  </head>
+  <body>
+  
+  <header class="header">
+  <div class="container">
+      <div class="btn-menu">
+          <!-- En el index.html -->
+<label id="milabel" class="menu-icon-index" for="btn-menu"><i class="fas fa-bars"></i></label>
+      </div>
+      <div class="logo">
+          <a href="index.php">
+              <img style="width: 170px" src="" alt="">
+          </a>
+          <h2>Blog</h2>
+  
+              </div>
+          </div> <!-- contenedor -->
+      </header>
+      <div class="centrar-texto">
+      <br><br><br><br>
+          <h1 class="fw-700 centrar-texto"><?php echo $titulo  ?> </h1>
+          <div class="contenedor seccion contenido-centrado">
+          <br>
+           
+        </div>
+          <img src="<?php echo $rutaDestino ?> " alt="imagen rota">
+      </div>
+      <br>
+          <center><p><?php echo nl2br($descripcionImg);  ?></center> </p>
+          <br>
+          <center><meta name= "description" content = "<?php echo nl2br($descripcion);  ?>"><?php echo nl2br($descripcion);  ?></center><br>
+      
+  
+      <main class="contenedor seccion contenido-centrado">
+          <div class="resumen-propiedad">
+          <center><meta name= "author" content = "<?php echo nl2br($autor);  ?>"><?php echo nl2br($autor);  ?></center><br>
+             
+          </div><!--.resumen-propiedad-->
+  
+          <p><?php echo nl2br($contenido);  ?> </p>
+          <br>
+          <center><meta name= "keywords" content = "<?php echo nl2br($keywords);  ?>"><?php echo nl2br($keywords);  ?></center><br>
+          
+  
+      </main>
+  
+      <footer class="site-footer seccion">
+          
+          </div>
+      </footer>
+  </body>
+  </html>
---
+++ blogs/prueba_numero_1.php
@@ -0,0 +1,76 @@
+<?php
+$titulo = "Prueba numero 1";
+$descripcionImg = "imagen sin contexto del titulo";
+$contenido = "<div>ahora si ya?</div>";
+
+$descripcion = "Las demas pruebas nunca existieron, gracias";
+
+$rutaDestino = "../img/175496196-día-de-prevención-de-desastres-naturales-con-niños-explicando-guía-de-seguridad.jpg";
+
+$autor = "Victor Luna";
+$keywords = "prueba, texto, ya";
+$estado = "CDMX";
+?> 
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title><?php echo $titulo ?></title>
+      <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+  
+      <link rel="stylesheet" href="../css/styles.css">
+  </head>
+  <body>
+  
+  <header class="header">
+  <div class="container">
+      <div class="btn-menu">
+          <!-- En el index.html -->
+<label id="milabel" class="menu-icon-index" for="btn-menu"><i class="fas fa-bars"></i></label>
+      </div>
+      <div class="logo">
+          <a href="index.php">
+              <img style="width: 170px" src="" alt="">
+          </a>
+          <h2>Blog</h2>
+  
+              </div>
+          </div> <!-- contenedor -->
+      </header>
+      <div class="centrar-texto">
+      <br><br><br><br>
+          <h1 class="fw-700 centrar-texto"><?php echo $titulo  ?> </h1>
+          <div class="contenedor seccion contenido-centrado">
+          <br>
+           
+        </div>
+          <img src="<?php echo $rutaDestino ?> " alt="imagen rota">
+      
+      <br>
+          <p><?php echo nl2br($descripcionImg);  ?> </p>
+          <br>
+          <center><meta name= "description" content = "<?php echo nl2br($descripcion);  ?>"><?php echo nl2br($descripcion);  ?><center><br>
+      
+  
+      <main class="contenedor seccion contenido-centrado">
+          <center><meta name= "author" content = "<?php echo nl2br($autor);  ?>"><?php echo nl2br($autor);  ?><center><br>
+          <p><?php echo nl2br($contenido);  ?> </p>
+          <br>
+          <meta name= "keywords" content = "<?php echo nl2br($keywords);  ?>"><?php echo nl2br($keywords);  ?><br>
+          </div>
+  
+      </main>
+  
+      <footer class="site-footer seccion">
+          <div class="contenedor contenedor-footer">
+              <nav class="navegacion">
+                  <a href="#">Quiénes somos</a>
+                  <a href="#">Contacto</a>
+              </nav>
+              <p class="copyright">Todos los Derechos Reservados 2023 &copy; </p>
+          </div>
+      </footer>
+  </body>
+  </html>
